b9a862b840445eb54e48cdbd4c6a813d9a75d213
dattapadal@duck.com <dattapadal@duck.com>
2023-02-20 05:51:22 +0000

    Week 4 answers part 2

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -1,20 +1,17 @@
-{{ config(materialized='view') }}
+{{ config(materialized='view')}}
 
-select
-    int64_field_0 as Id,
-    dispatching_base_num,
+select 
+    cast(dispatching_base_num as string) as dispatching_base_num,
     cast(pickup_datetime as timestamp) as pickup_datetime,
     cast(dropOff_datetime as timestamp) as dropoff_datetime,
-    cast(pulocationid as integer) as  pickup_locationid,
-    cast(dolocationid as integer) as dropoff_locationi,
-    cast(SR_Flag as integer) as SR_Flag,
-    Affiliated_base_number
-from
-    {{ source('staging','fhv_data_2019') }}
-    
--- dbt build --m <model.sql> --var 'is_test_run: false'
+    cast(PUlocationID as integer) as pickup_locationid,
+    cast(DOlocationID as integer) as dropoff_locationid,
+    cast(SR_Flag as numeric) as sr_flag,
+    cast(Affiliated_base_number as string) as affiliated_base_number
+from {{ source('staging', 'fhv_data_2019')}}
+
 {% if var('is_test_run', default=true) %}
 
-  limit 100
+limit 100
 
 {% endif %}
